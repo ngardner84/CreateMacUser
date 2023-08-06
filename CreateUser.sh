9ec511ebd518183a628a38ca6fd12cde435a76fe
Nathan Gardner <38595421+ngardner84@users.noreply.github.com>
2023-08-06 13:29:57 -0700

    Update CreateUser.sh

--- CreateUser.sh
+++ CreateUser.sh
@@ -11,12 +11,26 @@ USERNAME="Temp User"
 USERID="501"
 PASSWORD="tempPassword"
 
+# Function to check if the USERID exists
+user_id_exists() {
+  if dscl . -list /Users UniqueID | awk '{print $2}' | grep -q "^$1$"; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+# Increment USERID if it already exists
+while user_id_exists $USERID; do
+  USERID=$((USERID + 1))
+done
+
 # Create the user
 sysadminctl -addUser "$USERNAME" -password "$PASSWORD" -UID "$USERID" -fullName "$USERNAME" -home "/Users/$USERNAME" -admin
 
 # Check if the user was created successfully
 if [ $? -eq 0 ]; then
-  echo "User '$USERNAME' created successfully."
+  echo "User '$USERNAME' created successfully with UID $USERID."
 else
   echo "Failed to create user '$USERNAME'."
 fi
